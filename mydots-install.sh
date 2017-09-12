3c108ed989372814ddd98ebba9931daec56487b9
root <root@jayabal-lnx.novalocal>
2017-09-12 09:09:40 -0700

    update installer script

--- mydots-install.sh
+++ mydots-install.sh
@@ -1,4 +1,6 @@
 echo "export MYDOTS=`pwd`" >> $HOME/.bashrc
+source $HOME/.bashrc
 source $MYDOTS/.dots/bashrc
+yum install mosh ctags cscope tmux
 git clone https://github.com/gmarik/Vundle.vim.git .dots/vim/bundle/Vundle.vim
 vim +PluginInstall +qall
